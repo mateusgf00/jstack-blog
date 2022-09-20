b2db61dd25bf9b5c5c6d035af6d5f8838d6df1fc
Mateus <mateusgf00@gmail.com>
2022-09-20 10:18:04 -0300

    alter theme use useMemo and useState

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Footer() {
+export default function Footer({onToggleTheme}) {
   return (
     <Container>
       <span>JStack's Blog. Todos os direitos reservados.</span>
-      <button type="button">🌞</button>
+      <button
+        type="button"
+        onClick={onToggleTheme}
+      >🌞</button>
     </Container>
   );
 }
