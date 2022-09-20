a5a0bb524b8a3ff207bad5af1cbd1a722107428f
Mateus <mateusgf00@gmail.com>
2022-09-20 10:01:49 -0300

    alter the global styles to dark and light theme

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   body {
-    background: ${props => props.theme.backGroundColor};
+    background: ${props => props.theme.backgroundColor};
     color: ${props => props.theme.textColor};
     font-family: sans-serif;
   }
