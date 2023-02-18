9013711977a28f4d4d568d8e10a674f47951570d
Mateus <mateusgf00@gmail.com>
2023-02-17 21:36:49 -0300

    aprendendo a usar e manipular queryParams

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -13,6 +13,7 @@ export default function Layout() {
       <Nav>
         <Link to='/'>Home</Link>
         <Link to='/posts'>Posts</Link>
+        <Link to='/posts/:id'>Post</Link>
       </Nav>
       <Routes />
       <Footer />
